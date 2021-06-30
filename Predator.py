26963f9a5b798d3d950554bbf2c75b8b947f2266
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-06-30 21:01:56 +0530

    Update Predator.py

--- Predator.py
+++ Predator.py
@@ -450,7 +450,7 @@ async def _(e):
 @ddk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
 async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
-    error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
+    error = "Aukat se bhat mat jao bhiya🙂❤️."
     if e.sender_id in SMEX_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
@@ -459,12 +459,12 @@ async def spam(e):
         if len(predator) == 2:
             message = str(predator[1])
             counter = int(predator[0])
-            if counter > 100:
+            if counter > 1000:
                 return await e.reply(error, parse_mode=None, link_preview=None )
             await asyncio.wait([e.respond(message) for i in range(counter)])
         elif e.reply_to_msg_id and smex.media:  
             counter = int(predator[0])
-            if counter > 100:
+            if counter > 1000:
                 return await e.reply(error, parse_mode=None, link_preview=None )
             for _ in range(counter):
                 smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
@@ -472,7 +472,7 @@ async def spam(e):
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
             counter = int(predator[0])
-            if counter > 100:
+            if counter > 1000:
                 return await e.reply(error, parse_mode=None, link_preview=None )
             await asyncio.wait([e.respond(message) for i in range(counter)])
         else:
